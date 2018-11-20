59a0132a38da682e5611f9687d82a47ae34f14cf
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-11-21 00:57:45 +0100

    ..

--- python/cov_svd.py
+++ python/cov_svd.py
@@ -13,3 +13,17 @@ XT = np.random.rand(10,10)
 
 cref = np.cov(XT, rowvar=False)
 c = mycov(XT)
+
+u,s,v = np.linalg.svd(XT, full_matrices=False)
+XTinv = np.linalg.inv(XT)
+diff = XTinv - np.dot(np.dot(v.T, np.linalg.inv(np.diag(s))), u.T)
+
+print(diff.max())
+
+norm = np.dot(np.ones((1,10)),np.dot(XTinv, np.ones((10,1))))
+
+# MatrixXd centered = mat.rowwise() - mat.colwise().mean();
+# MatrixXd cov = (centered.adjoint() * centered) / double(mat.rows() - 1);
+# Map<Matrix<double,Dynamic,21,RowMajor> > mat(&(all_data[0][0], all_data.size(), 21);
+
+print(norm)
